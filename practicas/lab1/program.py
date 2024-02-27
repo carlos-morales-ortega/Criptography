15966a564af4dcc198e5c194451f8ff04c4a12cb
carlos-morales-ortega <carlos.morales.ortega@comunidad.unam.mx>
2024-02-27 02:28:30 -0600

    Cifrado completo

--- practicas/lab1/program.py
+++ practicas/lab1/program.py
@@ -1,31 +1,78 @@
 #Hill Cipher
 #
 #author: Morales Ortega Carlos
-import fileinput
+import fileinput, math
 
 #Variables a utilizar
-do=[]
-key=[]
-plain_text=[]
-matrix_key = [['',''],['','']]
+lines = []
 
-# Métodos 
-def key_c():
-    matrix_key[0][0] = key[0]
-    matrix_key[0][1] = key[1]
-    matrix_key[1][0] = key[2]
-    matrix_key[1][1] = key[3]
-    print(matrix_key)
+letras={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,
+        'L':11,'M':12,'N':13,'Ñ':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,
+        'U':20,'V':21,'W':22,'X':23,'Y':24,'Z':25}
 
-def cipher():
-    key_c()
+################################################################################
+def do_matrix(to_matrix):
+    
+    matrix=[]
+    aux=[]
+    for i in range (2):
+        aux=[]
+        for j in range (2):
+            aux.append(letras.get(to_matrix[2 * j + i]))
+        matrix.append(aux)
+    return matrix
+
+def do_vector(to_matrix):
+    
+    vector=[]
+    for i in range(0,len(to_matrix),2):
+        aux_vector=[]
+        for j in range (2):
+            aux_vector.append(letras.get(to_matrix[i+j]))
+        vector.append(aux_vector)
+    return vector
+
+def mxv(matrix, vector):
+    
+    resultado=[]
+    for aux in vector: 
+        aux_resultado=[]
+        for i in range (2):
+            mod=0
+            for j in range (2):
+                mod += matrix[i][j] * aux[j]
+            aux_resultado.append(mod % 26) 
+        resultado.append(aux_resultado)
+    
+    return resultado
+
+def msg(matrix):
+    message = ''
+    for vector in matrix:
+        for x in vector:
+            for key,value in letras.items():
+                if x==value:
+                    message+=key
+    
+    return message
+
+def cipher(key, plain_text):
+    matrix_key= do_matrix(key)
+    aux_vector= do_vector(plain_text)
+    final=mxv(matrix_key,aux_vector)
+    d=msg(final)
+    print(d)
     
 
 def decipher():
     print(lines)
     print("Se va a descifrar")
 
-def limpia():
+def start(): #This method put the inputs in list withou \n
+    do=[]
+    key=[]
+    plain_text=[]
+
     for i in lines[0]:
         aux=i.replace('\n','')
         do.append(aux.upper() )
@@ -35,32 +82,31 @@ def limpia():
         aux=i.replace('\n','')
         plain_text.append(aux.upper() )
     plain_text.remove('')
+    if(len(plain_text)%2!=0):
+        plain_text.append('X')
+    print(plain_text)
 
     for i in lines[2]:
         aux=i.replace('\n','')
         key.append( aux.upper() )
     key.remove('')
 
-# Lectura de argumentos e inicio de programa
+    if(do[0] =='C'):
+        cipher(key,plain_text)
+    elif (do[0] == 'D'):
+        decipher()
+    else: 
+        print("Invalid input")
+################################################################################
+    
+# Input reading and start of the program
 
-lines = []
 for line in fileinput.input():
     lines.append(line)
 
 # do something with the lines array
-if(len(lines)==3):
-    
-    limpia()
-    if(do[0] =='C'):
-        cipher()
-    elif (do[0] == 'D'):
-        decipher()
-    else: 
-        print(do)
-        print(plain_text)
-        print(key)
-        print("Invalid input")
+if(len(lines)==3):  
+    start()
 else:
-    print(len(lines))
     print("Invalid inputs")
 
